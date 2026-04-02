ceb595178a51228277bbd35e9b00f182d6608e14
KDWBadSinger <ethanwong258@gmail.com>
2026-04-02 00:24:50 -0700

    Update HUD and Win Panel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,61 +3,67 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-	public PLAYERSTATE currentState; // Current state of the player (Body or Soul)
+    public PLAYERSTATE currentState; // Current state of the player (Body or Soul)
 
-	public GameObject body; // Reference to the body GameObject
-	public GameObject soul; // Reference to the soul GameObject
+    public GameObject body; // Reference to the body GameObject
+    public GameObject soul; // Reference to the soul GameObject
 
-	void Update()
-	{
-		if (Keyboard.current != null && Keyboard.current.qKey.wasPressedThisFrame)
-		{
-			// Switch player state when Q is pressed
-			HandleSwitch();
-		}
-	}
+    public HUDManager hudManager; // NEW
 
-	void HandleSwitch()
-	{
-		// Determine which state to switch to
-		if (currentState == PLAYERSTATE.BODY)
-		{
-			EnterShadow(); // Switch to Soul form
-		}
-		else
-		{
-			EnterBody(); // Switch to Body form
-		}
-	}
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            //Debug.Log("Q pressed, currentState = " + currentState);
+            HandleSwitch();
+        }
+    }
 
-	void EnterShadow()
-	{
-		// Stop body movement and disable its PlayerMove script
-		body.GetComponent<PlayerMove>().Stop();
-		body.GetComponent<PlayerMove>().enabled = false;
+    void HandleSwitch()
+    {
+        if (currentState == PLAYERSTATE.BODY)
+        {
+            EnterShadow();
+        }
+        else
+        {
+            EnterBody();
+        }
+    }
 
-		// Set state to Soul and position soul at body's location
-		currentState = PLAYERSTATE.Soul;
-		soul.transform.position = body.transform.position;
-		soul.SetActive(true);
+    void EnterShadow()
+    {
+        //Debug.Log("EnterShadow called");
 
-		// Enable movement for the soul
-		soul.GetComponent<PlayerMove>().enabled = true;
-	}
+        body.GetComponent<PlayerMove>().Stop();
+        body.GetComponent<PlayerMove>().enabled = false;
 
-	void EnterBody()
-	{
-		// Stop soul movement and disable its PlayerMove script
-		soul.GetComponent<PlayerMove>().Stop();
-		soul.GetComponent<PlayerMove>().enabled = false;
+        currentState = PLAYERSTATE.Soul;
+        soul.transform.position = body.transform.position;
+        soul.SetActive(true);
 
-		// Set state to Body and swap positions of body and soul
-		currentState = PLAYERSTATE.BODY;
-		Vector2 pos = body.transform.position;
-		body.transform.position = soul.transform.position;
-		soul.transform.position = pos;
+        soul.GetComponent<PlayerMove>().enabled = true;
 
-		// Enable movement for the body
-		body.GetComponent<PlayerMove>().enabled = true;
-	}
+        //Debug.Log("Calling HUD with state = " + currentState);
+        hudManager?.UpdateFormUI(PLAYERSTATE.Soul);
+    }
+
+
+    void EnterBody()
+    {
+        //Debug.Log("EnterBody called");
+
+        soul.GetComponent<PlayerMove>().Stop();
+        soul.GetComponent<PlayerMove>().enabled = false;
+
+        currentState = PLAYERSTATE.BODY;
+        Vector2 pos = body.transform.position;
+        body.transform.position = soul.transform.position;
+        soul.transform.position = pos;
+
+        body.GetComponent<PlayerMove>().enabled = true;
+
+        //Debug.Log("Calling HUD with state = " + currentState);
+        hudManager?.UpdateFormUI(PLAYERSTATE.BODY);
+    }
 }
